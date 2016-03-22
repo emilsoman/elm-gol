6d4d277dcddfc7ce5d6ddafbe1e983799300ab59
Emil Soman <emil.soman@gmail.com>
2016-03-22 22:29:30 +0530

    Try colors and Text module

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,13 @@
 module Main where
 import Html exposing (text)
+import Color exposing (rgb)
+import Text
+import Graphics.Element exposing (..)
 
 main =
-  text "Hello world"
+  Text.fromString "Hello World"
+      |> Text.color (rgb 52 152 219)
+      |> Text.italic
+      |> Text.bold
+      |> Text.height 60
+      |> leftAligned
