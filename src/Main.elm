3a14a8c4a27cad69a84d7944c2b92f751612fa85
Emil Soman <emil.soman@gmail.com>
2016-03-25 16:16:41 +0530

    Use window dimensions signal to create grid

--- src/Main.elm
+++ src/Main.elm
@@ -1,13 +1,42 @@
-module Main where
-import Html exposing (text)
-import Color exposing (rgb)
-import Text
+import Debug
 import Graphics.Element exposing (..)
+import Window
 
+cellSize = 30
+type alias Grid = List (List Bool)
+
+-- Helper that debugs a signal
+debugSignal name signal =
+  Signal.map (Debug.watch name) signal
+
+-- Calculates the grid dimensions (in terms of cell count)
+-- based on the dimensions of the container
+gridDimension : Signal (Int, Int)
+gridDimension =
+  Window.dimensions
+  |> Signal.map (\(w,h) -> ((w // cellSize), (h // cellSize)))
+
+-- Returns the initial state of the Grid
+--seed : Signal (Int, Int) -> Signal Grid
+seed : (Int, Int) -> Grid
+seed (w, h) =
+  List.repeat h (List.repeat w False)
+
+-- Given a state of the grid, draws an element
+-- to represent its state
+drawGrid : Grid -> Element
+drawGrid grid =
+  show grid
+
+--Function that accepts a grid of boolean values
+--and generates the next step in the lifetime of the grid
+step : Grid -> Grid
+step list =
+  list
+
+main : Signal Element
 main =
-  Text.fromString "Hello World"
-      |> Text.color (rgb 52 152 219)
-      |> Text.italic
-      |> Text.bold
-      |> Text.height 60
-      |> leftAligned
+  gridDimension
+  |> debugSignal "Gridsize"
+  |> Signal.map seed
+  |> Signal.map drawGrid
